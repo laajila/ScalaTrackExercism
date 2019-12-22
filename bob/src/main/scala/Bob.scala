e53f422c1d7e816d8e078fcae1b743d31a3b6d4c
Issam Benabachir <issam.benabachir@gmail.com>
2019-12-22 17:29:24 +0100

    bob solution

--- bob/src/main/scala/Bob.scala
+++ bob/src/main/scala/Bob.scala
@@ -1,13 +1,15 @@
-
-
 object Bob {
 
 
   def response(statement: String): String = statement match {
-    case _ if (statement.endsWith("?") && statement.toUpperCase.equals(statement) && (statement.dropRight(1).forall(!_.isDigit))) => "Calm down, I know what I'm doing!"
-    case _ if (statement.contains("?")) => "Sure."
+    case _ if (statement.trim.endsWith("?")
+      && statement.toUpperCase.equals(statement)
+      && statement.dropRight(1).exists(_.isLetter)) => "Calm down, I know what I'm doing!"
+    case _ if (statement.trim.endsWith("?")) => "Sure."
     case _ if (statement.trim.isEmpty) => "Fine. Be that way!"
-    case _ if (statement.toUpperCase.equals(statement)) => "Whoa, chill out!"
+    case _ if (statement.toUpperCase.equals(statement)
+      && statement.replaceAll(",", "")
+      .exists(_.isLetter)) => "Whoa, chill out!"
     case _ => "Whatever."
 
   }
