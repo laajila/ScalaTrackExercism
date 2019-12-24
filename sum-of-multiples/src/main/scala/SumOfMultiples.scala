5ecf68ffa8038a7947788f46229ee702c5ab9d0f
Issam Benabachir <issam.benabachir@gmail.com>
2019-12-24 14:56:59 +0100

    multiple sum

--- sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,4 +1,11 @@
 object SumOfMultiples {
-  def sum(factors: Set[Int], limit: Int): Int = ???
+
+  def sum(factors: Set[Int], limit: Int): Int = {
+    val listtoFindMultiplier = List.range(1, limit)
+    factors.flatMap(
+      i => listtoFindMultiplier.map(m => m*i).filter(_<limit)
+    ).sum
+
+  }
 }
 
