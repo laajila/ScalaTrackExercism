ba2a64a5a3a318d5d74cb8e06c598d0e59076ad5
Issam Benabachir <issam.benabachir@gmail.com>
2019-12-26 20:58:24 +0100

    armstrong num exo and sol

---
+++ armstrong-numbers/src/main/scala/ArmstrongNumbers.scala
@@ -0,0 +1,18 @@
+import scala.math
+object ArmstrongNumbers {
+
+  def isArmstrongNumber(n: Int): Boolean = {
+    digits(n).map(scala.math.pow(_,numDigits(n)).asInstanceOf[Int]).sum==n
+  }
+
+  def numDigits(n: Int, nDigits: Int = 0): Int = n match {
+    case _ if(n/10==0) => nDigits +1
+    case _ => numDigits(n / 10, nDigits + 1)
+  }
+
+  def digits(n: Int, list: List[Int] = List()): List[Int] = n % 10 match {
+    case 0 => list :+ n / (numDigits(n) * 10)
+    case _ => digits(n / 10, list :+ n % 10)
+
+  }
+}
