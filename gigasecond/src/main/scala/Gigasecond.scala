1c90667772f49fee1a81974b5c1469324c246c7c
Issam Benabachir <issam.benabachir@gmail.com>
2019-12-28 13:19:21 +0100

    gigaseconds solution

--- gigasecond/src/main/scala/Gigasecond.scala
+++ gigasecond/src/main/scala/Gigasecond.scala
@@ -2,7 +2,7 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 
 object Gigasecond {
-  def add(startDate: LocalDate): LocalDateTime = ???
+  def add(startDate: LocalDate): LocalDateTime = startDate.atStartOfDay().plusSeconds(scala.math.pow(10,9).toInt)
 
-  def add(startDateTime: LocalDateTime): LocalDateTime = ???
+  def add(startDateTime: LocalDateTime): LocalDateTime = startDateTime.plusSeconds(scala.math.pow(10,9).toInt)
 }
