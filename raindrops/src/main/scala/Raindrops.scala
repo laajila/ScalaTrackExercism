941a72f6caa2ac9b9709033fa83ff4284928a7ac
Issam Benabachir <issam.benabachir@gmail.com>
2019-12-29 09:27:47 +0100

    raindrops solution

--- raindrops/src/main/scala/Raindrops.scala
+++ raindrops/src/main/scala/Raindrops.scala
@@ -1,4 +1,11 @@
 object Raindrops {
-  def convert(n: Int): String = ???
+  def convert(n: Int,raindrop:String=""): String = n match {
+    case _ if(n%3==0 && !raindrop.contains("Pling")) => convert(n/3,raindrop+"Pling")
+    case _ if(n%5==0 && !raindrop.contains("Plang")) => convert(n/5,raindrop+"Plang")
+    case _ if(n%7==0 &&  !raindrop.contains("Plong")) => convert(n/7,raindrop+"Plong")
+    case _ if(raindrop=="") => n.toString
+    case _ => raindrop
+
+  }
 }
 
